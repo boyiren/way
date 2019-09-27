80bbad093a23afa9fd6338c1d3dbca4ff7ca99cb
boyiren <17825270377@163.com>
2019-09-28 00:17:33 +0800

    我的道路，就从这里开始吧

---
+++ src/main/java/my/way/Application.java
@@ -0,0 +1,17 @@
+package my.way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author boyiren
+ * @data 2019-09-28
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
